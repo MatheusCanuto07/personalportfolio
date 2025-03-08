7d35187869b44db11b675d28e219d5cd6578bf94
Matheuscanuto07 <matheushenriquecanuto77@gmail.com>
2025-03-08 17:56:58 -0300

    init

---
+++ src/lib/variables.ts
@@ -0,0 +1,42 @@
+// Set any item to undefined to remove it from the site or to use the default value
+
+export const GLOBAL = {
+  // Site metadata
+  username: "Matheus Canuto",
+  rootUrl: "https://zaggonaut.dev",
+  shortDescription: "Full Stack Web Developer </br> (C# and JavaScript)",
+  longDescription: "Welcome to my portfolio, built with Astro, TypeScript, and TailwindCSS. Learn more about my work!",
+  
+  // Social media links
+  githubProfile: "https://github.com/MatheusCanuto07",
+  linkedinProfile: "https://www.linkedin.com/in/matheus-canuto/",
+  
+  // Common text names used throughout the site
+  articlesName: "Articles",
+  projectsName: "Projects",
+  viewAll: "View All",
+  
+  // Common descriptions used throughout the site
+  noArticles: "No featured articles yet.",
+  noProjects: "No featured projects yet.",
+
+  // Blog metadata
+  blogTitle: "My Thoughts & Takes",
+  blogShortDescription: "Practical wisdom, unfiltered thoughts, and hot takes.",
+  blogLongDescription: "Web development, tech trends, and the occasional programming mishap.",
+
+  // Project metadata
+  projectTitle: "Projects and Code",
+  projectShortDescription: "A list of my web development projects and developer tools.",
+  projectLongDescription: "All of my projects, including both frontend and full-stack applications.",
+
+  // Profile image
+  profileImage: "michael-dam-unsplash.webp",
+
+  // Menu items
+  menu: {
+    home: "/",
+    projects: "/projects",
+    blog: "/blog",
+  }
+};
